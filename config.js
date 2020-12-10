fc2d229e6597ed9cedf41b8dd42938f8a1aaf819
Nikita Danilkin <61296857+ElF649@users.noreply.github.com>
2020-12-10 16:19:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const {
-  DB_URL: 'mongodb://localhost:27017/diplomApi',
-  PORT: 3000,
+  DB_URL = 'mongodb://localhost:27017/diplomApi',
+  PORT = 3000,
   JWT_SECRET = 'JWT_SECRET',
 } = process.env;
 
